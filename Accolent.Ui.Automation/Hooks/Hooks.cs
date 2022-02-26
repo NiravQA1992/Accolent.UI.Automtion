2cc056c34ceac6b031c579e2c80c12ea6f1bbfc7
nirav <nirav@NEERVITA>
2022-02-26 13:43:58 +0530

    Extent report configuration

--- Accolent.Ui.Automation/Hooks/Hooks.cs
+++ Accolent.Ui.Automation/Hooks/Hooks.cs
@@ -1,6 +1,103 @@
-﻿namespace Accolent.Ui.Automation.Hooks
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
+using BoDi;
+using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Accolent.Ui.Automation.Hooks
 {
-    class Hooks
+    [Binding]
+    public class Hooks
     {
+        private static ExtentTest featureName;
+        private static ExtentTest scenario;
+        private static ExtentReports extent;
+
+
+        private readonly IObjectContainer _objectContainer;
+
+        private IWebDriver driver;
+
+        public Hooks(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+        }
+
+        [BeforeTestRun]
+        public static void InitializeReport()
+        {
+            string directoryPath = AppDomain.CurrentDomain.BaseDirectory.Replace("\\bin\\Debug", "");
+            string reportPath = directoryPath + "Report\\AccolentAutomationTestResult.html";
+            ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(reportPath);
+            htmlReporter.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+        }
+
+        [BeforeFeature]
+        [Obsolete]
+        public static void BeforeFeature()
+        {
+            featureName = extent.CreateTest<Feature>(FeatureContext.Current.FeatureInfo.Title);
+        }
+
+        [BeforeScenario]
+        [Obsolete]
+        public void BeforeScenario()
+        {
+            Console.WriteLine("BeforeScenario");
+            scenario = featureName.CreateNode<Scenario>(ScenarioContext.Current.ScenarioInfo.Title);
+        }
+
+        [AfterStep]
+        [Obsolete]
+        public void InsertReportingSteps()
+        {
+            var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+            if (ScenarioContext.Current.TestError == null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "And")
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text);
+            }
+            else if (ScenarioContext.Current.TestError != null)
+            {
+                if (stepType == "Given")
+                {
+                     scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.Message);
+                }
+                else if (stepType == "When")
+                {
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.Message);
+                }
+                else if (stepType == "Then")
+                {
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.Message);
+                }
+                else if (stepType == "And")
+                {
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.Message);
+                }
+            }
+        }
+
+        [AfterTestRun]
+        public static void TearDownReport()
+        {
+            extent.Flush();
+        }
+
+        [AfterScenario]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
     }
 }
